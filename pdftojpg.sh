9dab66f23f59d9c378f415714b6a2c993e502568
YanDoroshenko <yan1994doroshenko@gmail.com>
2015-12-17 00:49:36 +0100

    Create pdftojpg.sh

---
+++ pdftojpg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Enter number of pages: ";
+read pages; # No idea for getting number of pages for now
+pages=$(($pages-1))  # Numeration starts from 0
+file=$1
+file="${file%.*}" # Removing extension
+mkdir "converted$file"
+dir="converted$file"
+for i in {0..$pages..1}; do
+    convert -density 500 $file["$i"].pdf  $dir/$file"$i".jpg;
+done;
+echo "Done!"
