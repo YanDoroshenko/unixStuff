1d97cc42dc1b751242324107c69be49ae430e84b
Yan Doroshenko <yan1994doroshenko@gmail.com>
2015-12-17 11:40:44 +0100

    Working version

--- pdftojpg.sh
+++ pdftojpg.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-###
-### NOT WORKING YET ###
-###
-
 echo "Enter number of pages: ";
 read pages; # No idea for getting number of pages for now
 pages=$(($pages-1))  # Numeration starts from 0
 file=$1
 file=$(basename "$file")
 file="${file%.*}" # Removing extension
-mkdir "converted$file"
-dir="converted$file"
+if [ ! -d "converted$file" ]; then
+    mkdir "converted$file";
+fi;
+dir="converted$file";
 for i in $(eval echo {0..$pages}); do
     convert -density 500 $file.pdf["$i"]  $dir/$file"$i".jpg;
 done;
-echo "Done!"
+echo "Done!";
