cd3f7a06fdba365ffa3ca72c4a6ca9727dd9f3e3
Yan Doroshenko <yan1994doroshenko@gmail.com>
2015-12-17 12:02:54 +0100

    Documentation + progress status

--- pdftojpg.sh
+++ pdftojpg.sh
@@ -2,6 +2,7 @@
 
 file=$1 # First cmd argument
 pages=`pdfinfo -meta $1 | grep Pages | cut -d' ' -f11` #Getting pages number from meta (-f 11 is stable, AFAIK)
+pages=$(($pages-1)) # Pages count from 0
 file=$(basename "$file") # Dropping path
 file="${file%.*}" # Removing extension
 dir="converted$file";
@@ -10,5 +11,6 @@ if [ ! -d "$dir" ]; then
 fi;
 for i in $(eval echo {0..$pages}); do
     convert -density 500 $file.pdf["$i"]  $dir/$file"$i".jpg; # Density for it to look readable
+    echo "Page $((i+1)) of $((pages+1)) converted" # Progress
 done;
 echo "Done!";
