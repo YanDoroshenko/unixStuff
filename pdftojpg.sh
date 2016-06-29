a2e5a8dd8b9c43e937d4f4b9baa68fca8689ab22
Yan Doroshenko <yan1994doroshenko@gmail.com>
2016-06-29 23:52:29 +0200

    Fixed directory structure, major refactoring, some checks added

--- pdftojpg.sh
+++ pdftojpg.sh
@@ -2,19 +2,23 @@
 
 file=$1 # First cmd argument
 if [ -z $file ]; then
-    echo "Usage: bash pdftojpg.sh yourfile.pdf";
-else
-    pages=`pdfinfo -meta $1 | grep Pages | cut -d' ' -f11` #Getting pages number from meta (-f 11 is stable, AFAIK)
-    pages=$(($pages-1)) # Pages count from 0
-    file=$(basename "$file") # Dropping path
-    file="${file%.*}" # Removing extension
-    dir="converted$file";
-    if [ ! -d "$dir" ]; then
-	mkdir "$dir";
-    fi;
-    for i in $(eval echo {0..$pages}); do
-	convert -density 500 $file.pdf["$i"]  $dir/"$file"_Page"$(($i+1))".jpg; # Density for it to look readable
-	echo "Page $((i+1)) of $((pages+1)) converted" # Progress
-    done;
-    echo "Done!";
-fi;
+    echo "Usage: bash pdftojpg.sh yourfile.pdf"
+    exit 1
+fi
+if [ ! -f $file ]; then 
+    echo "File does not exist"
+    exit 1
+fi
+pages=`pdfinfo -meta $file | grep Pages | cut -d' ' -f11` # Getting pages number from meta (-f 11 is stable, AFAIK)
+pages=$(($pages-1)) # Pages count from 0
+short=$(basename $file) # Dropping path
+short=${short%.*} # Removing extension
+dir="converted_$short"
+if [ ! -d $dir ]; then
+    mkdir $dir
+fi
+for i in $(eval echo {0..$pages}); do
+    convert -density 50 $file[$i]  $dir/"$short"_Page$(($i+1)).jpg # Density for it to look readable
+    echo "Page $((i+1)) of $((pages+1)) converted" # Progress
+done
+echo "Done!"
