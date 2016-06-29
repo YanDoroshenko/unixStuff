3f8f6a94c71575cff170650c5c01dd824340a4a1
Yan Doroshenko <yan1994doroshenko@gmail.com>
2016-06-30 00:09:49 +0200

    Added density select

--- pdftojpg.sh
+++ pdftojpg.sh
@@ -2,12 +2,26 @@
 
 file=$1 # First cmd argument
 if [ -z $file ]; then
-    echo "Usage: bash pdftojpg.sh yourfile.pdf"
+    echo "Usage: bash pdftojpg.sh yourfile.pdf">&2
     exit 1
 fi
 if [ ! -f $file ]; then 
-    echo "File does not exist"
+    echo "File does not exist">&2
     exit 1
+fi
+echo "Enter density (100-2000), higher density -> better quality, slower convertation. Default - 300"
+read density
+if ! [[ $density =~ ^[0-9]+$ ]]; then
+    density=300
+else
+    if [ $density -lt 100 ]; then
+	echo "Density is too low, text will be unreadable">&2
+	exit 1
+    fi
+    if [ $density -gt 2000]; then
+	echo "Density is too high, converting will take forever">&2
+	exit 1
+    fi
 fi
 pages=`pdfinfo -meta $file | grep Pages | cut -d' ' -f11` # Getting pages number from meta (-f 11 is stable, AFAIK)
 pages=$(($pages-1)) # Pages count from 0
@@ -17,8 +31,9 @@ dir="converted_$short"
 if [ ! -d $dir ]; then
     mkdir $dir
 fi
+echo "Starting convertation with density $density" 
 for i in $(eval echo {0..$pages}); do
-    convert -density 500 $file[$i]  $dir/"$short"_Page$(($i+1)).jpg # Density for it to look readable
+    convert -density $density $file[$i]  $dir/"$short"_Page$(($i+1)).jpg # Density for it to look readable
     echo "Page $((i+1)) of $((pages+1)) converted" # Progress
 done
 echo "Done!"
