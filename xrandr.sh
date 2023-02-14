2d6d647531e7aa321eb7e8acd608328a3ebf4dfb
Yan Doroshenko <yan1994doroshenko@gmail.com>
2023-02-14 14:13:42 +0100

    [upd] Update xrandr script

--- xrandr.sh
+++ xrandr.sh
@@ -4,5 +4,5 @@ sleep $1
 
 HDMI=$(xrandr | grep "HDMI-[0-9].* connected" | cut -d' ' -f1)
 
-[ -n "$HDMI" ] && xrandr --output $HDMI --auto --right-of eDP-1-1 --output eDP-1-1 --auto || xrandr --auto
+[ -n "$HDMI" ] && xrandr --output eDP-1 --auto --output $HDMI --auto --right-of eDP-1 --primary
 } &
