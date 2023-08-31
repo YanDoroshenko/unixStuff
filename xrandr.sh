b741ad00d0e79956ec151ac8ba3149baaa716224
Yan Doroshenko <yan1994doroshenko@gmail.com>
2023-08-31 10:27:56 +0200

    [fix] Fix screen initialization

--- xrandr.sh
+++ xrandr.sh
@@ -3,10 +3,11 @@
 sleep $1
 
 HDMI=$(xrandr | grep "HDMI-[0-9].* connected" | cut -d' ' -f1)
-DP_TYPE_C=$(xrandr | grep "DP-[0-9].* connected" | cut -d' ' -f1)
+DP_TYPE_C=$(xrandr | grep "^DP-[0-9].* connected" | cut -d' ' -f1)
 
 [ -n "$HDMI" -a -n "$DP_TYPE_C" ] && xrandr --output eDP-1 --auto --output HDMI-1-0 --primary --auto --right-of eDP-1 --output DP-1 --auto --right-of HDMI-1-0
-[ -n "$HDMI" -a -z "$DP_TYPE_C" ] && xrandr --output eDP-1 --auto --output $HDMI --auto --right-of eDP-1 --primary
-[ -n "$DP_TYPE_C" -a -z "$HDMI" ] && xrandr --output $DP_TYPE_C --auto --right-of $HDMI
+[ -n "$HDMI" -a -z "$DP_TYPE_C" ] && xrandr --output eDP-1 --auto --output "$HDMI" --auto --right-of eDP-1 --primary
+[ -n "$DP_TYPE_C" -a -z "$HDMI" ] && xrandr --output "$DP_TYPE_C" --auto --right-of "$HDMI"
+[ -z "$DP_TYPE_C" -a -z "$HDMI" ] && xrandr --output eDP-1 --auto
 } &
 
