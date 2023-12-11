f8a6fe4880ab571a8bef65b7576b36e45a3807c7
Yan Doroshenko <yandoroshenko@protonmail.com>
2023-12-11 14:51:19 +0100

    [upd] Improve battery charge profile handling

--- battery.sh
+++ battery.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-if [ -z "$(doas -n /sbin/system76-power charge-thresholds | grep "full_charge")" ]; then
-    doas -n /sbin/system76-power charge-thresholds --profile full_charge
-    notify-send "Battery charging to full"
-else
-    doas -n /sbin/system76-power charge-thresholds --profile max_lifespan
+CHARGE_PROFILE_FILE="/etc/charge_profile"
+
+if [ "$(cat $CHARGE_PROFILE_FILE)" = "full_charge" ]; then
+    doas -n /sbin/system76-power charge-thresholds --profile max_lifespan &&
+    echo "max_lifespan" > $CHARGE_PROFILE_FILE &&
     notify-send "Conserving battery lifetime"
+else
+    doas -n /sbin/system76-power charge-thresholds --profile full_charge &&
+    echo "full_charge" > $CHARGE_PROFILE_FILE &&
+    notify-send "Battery charging to full"
 fi
 
