00d12f659a95944f0d0953d785f5efb302eec8d4
Yan Doroshenko <yandoroshenko@protonmail.com>
2023-12-15 15:59:34 +0100

    [upd] Refactoring

--- layout.sh
+++ layout.sh
@@ -6,7 +6,8 @@ eDP=$(xrandr | grep "eDP-[0-9].* connected" | cut -d' ' -f1)
 HDMI=$(xrandr | grep "HDMI-[0-9].* connected" | cut -d' ' -f1)
 DP=$(xrandr | grep "^DP-[0-9].* connected" | cut -d' ' -f1)
 
-[ "$1" -eq 0 ] && xrandr --output "$DP" --off
+[ "$1" -eq 0 -a -n "$DP" ] && xrandr --output "$DP" --off
+[ "$1" -eq 0 -a -z "$DP" ] && xrandr --output "$eDP" --off
 
 [ -n "$HDMI" -a -n "$DP" ] && xrandr --output "$eDP" --auto --output "$DP" --primary --auto --right-of "$eDP" --output "$HDMI" --auto --right-of "$DP"
 [ -n "$HDMI" -a -z "$DP" ] && xrandr --output "$eDP" --auto --output "$HDMI" --auto --right-of "$eDP" --primary
