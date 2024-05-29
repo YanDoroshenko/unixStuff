1d01b9ddaf198ef492f280adc54f2a19982c1364
Yan Doroshenko <yandoroshenko@protonmail.com>
2024-05-29 11:05:37 +0200

    [upd] Improve wallpaper handling

--- layout.sh
+++ layout.sh
@@ -6,16 +6,13 @@ eDP=$(xrandr | grep "eDP-[0-9].* connected" | cut -d' ' -f1)
 HDMI=$(xrandr | grep "HDMI-[0-9].* connected" | cut -d' ' -f1)
 DP=$(xrandr | grep "^DP-[0-9].* connected" | cut -d' ' -f1)
 
-[ "$1" -eq 0 -a -n "$DP" ] && xrandr --output "$DP" --off
-[ "$1" -eq 0 -a -z "$DP" ] && xrandr --output "$eDP" --off
+[ "$1" -eq 0 ] && xrandr $(xrandr --listactivemonitors | awk '{print " --output "; print $4; print " --off"}' ORS='')
 
 [ -n "$HDMI" -a -n "$DP" ] && xrandr --output "$eDP" --auto --output "$DP" --primary --auto --right-of "$eDP" --output "$HDMI" --auto --right-of "$DP"
 [ -n "$HDMI" -a -z "$DP" ] && xrandr --output "$eDP" --auto --output "$HDMI" --auto --right-of "$eDP" --primary
 [ -n "$DP" -a -z "$HDMI" ] && xrandr --output "$eDP" --auto --right-of "$eDP"
 [ -z "$DP" -a -z "$HDMI" ] && xrandr --output "$eDP" --auto
 
-feh --bg-scale "/usr/share/slock/background.jpeg"
-
-systemctl --user restart pipewire
+feh --bg-scale "/usr/share/slock/background_full.jpeg"
 } &
 
