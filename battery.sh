eb74794e1946b63c212f44582cede4f6c367cdf6
Yan Doroshenko <yandoroshenko@protonmail.com>
2025-06-05 11:06:07 +0200

    [new] Add load battery state param

--- battery.sh
+++ battery.sh
@@ -4,7 +4,9 @@ CHARGE_PROFILE_FILE="$(realpath .)/.config/.charge_profile"
 
 touch $CHARGE_PROFILE_FILE
 
-if [ "$(cat $CHARGE_PROFILE_FILE)" = "full_charge" ]; then
+if [ "$1" = "-init" ]; then
+    system76-power charge-thresholds --profile $(cat $CHARGE_PROFILE_FILE)
+elif [ "$(cat $CHARGE_PROFILE_FILE)" = "full_charge" ]; then
     doas -n /sbin/system76-power charge-thresholds --profile max_lifespan &&
     echo "max_lifespan" > $CHARGE_PROFILE_FILE &&
     notify-send "Conserving battery lifetime"
